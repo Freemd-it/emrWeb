a6c4e1e5e11a23a3467fbf8bcac7f8438b7fc7ac
DongyeolLee <dongyeol8@gmail.com>
2017-07-28 19:50:36 +0900

    Update : receipt (접수 불러오기 완)

--- src/resources/js/receipt.js
+++ src/resources/js/receipt.js
@@ -57,6 +57,12 @@ $('#btn-name-send').on('click', () => {
             $('#name').val(name);
             $('#height').val('');
             $('#weight').val('');
+            $('#drinking').val('');
+            $('#smoking').val('');
+            $('#birth').val('');
+            $('#smokingPeriod').val('');
+            $('#drinkingPeriod').val('');
+            $('#bmi').val('');
 
             date = new Date();
             $('#firstcome').val(date.getFullYear() + ' 년 ' + (date.getMonth()+1) + ' 월 ' + date.getDate() + ' 일 ');
@@ -88,6 +94,15 @@ $('#btn-name-send').on('click', () => {
             $('#height').val(result[0].height);
 
             $('#weight').val(result[0].weight);
+
+            $('#drinking').val(result[0].smokingAmount);
+
+            $('#smoking').val(result[0].drinkingAmount);
+
+            $('#smokingPeriod').val(result[0].smokingPeriod);
+
+            $('#drinkingPeriod').val(result[0].drinkingPeriod);
+            $('#bmi').val(result[0].bmi);
 
             date = new Date(result[0].createdAt).toISOString().slice(0,10);
 
@@ -129,9 +144,77 @@ $('#btn-name-send').on('click', () => {
 });
 
 $(document).on('click', '.item', (e) => {
-    console.log(e.target.id);
+    let date;
+    const docs = {
+        id: e.target.id,
+    };
+
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:3000/receipt/patient',
+        data: docs,
+        dataType: 'json',
+        cache: false,
+    }).done((result) => {
+        $('#nameMessage').html('[ ' + result.name + ' ]' + ' 님이 조회됐습니다.');
+
+        $('#message').attr({
+            class: 'ui positive message',
+            hidden: false,
+        });
+
+        setTimeout(() => {
+            $('#message')
+                .closest('.message')
+                .transition('fade')
+        }, 1000);
+
+
+        $('#nameInput').val('');
+
+        $('#name').attr("disabled", true).val(result.name);
+
+
+        $('#height').val(result.height);
+
+        $('#weight').val(result.weight);
+
+        $('#drinking').val(result.smokingAmount);
+
+        $('#smoking').val(result.drinkingAmount);
+
+        $('#smokingPeriod').val(result.smokingPeriod);
+
+        $('#drinkingPeriod').val(result.drinkingPeriod);
+
+        $('#bmi').val(result.bmi);
+
+        date = new Date(result.createdAt).toISOString().slice(0,10);
+
+        $('#firstcome').val(date);
+
+        date = new Date(result.birth).toISOString().slice(0,10);
+
+        $('#birth').attr("disabled", true).val(date);
+
+        $('#gender').dropdown('set selected', result.gender);
+
+        $('.ui.dropdown').addClass("disabled");
+
+        $('.ui.basic.modal')
+            .modal('hide');
+    })
 });
 
 
 $('.ui.dropdown')
     .dropdown();
+
+$('#weight, #height').change(() => {
+    const weight = $('#weight').val();
+    const height = $('#height').val()/100;
+    const bmi = weight/(height*height);
+
+    $('#bmi').val(bmi.toFixed(5));
+});
+
